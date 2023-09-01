100836f224837e1c99e9f43e07083de09c69770f
JuanPQ <120750498+sebas1rus@users.noreply.github.com>
2023-09-01 16:24:21 -0500

    Savbe project

---
+++ Assets/Scripts/MainImageScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainImageScript : MonoBehaviour
+{
+    [SerializeField] private GameObject Unknow;
+    [SerializeField] private GameControllerScript gameController;
+
+    public void OnMouseDown()
+
+    {
+
+        if (Unknow.activeSelf && gameController.canOpen)
+        {
+            Unknow.SetActive(false);
+            gameController.imageOpened(this);
+        }
+    }
+
+    private int _spriteId;
+    public int spriteId
+    {
+        get { return _spriteId; }
+    }
+
+    public void ChangeSprite(int id, Sprite image)
+    {
+        _spriteId = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void Close()
+    {
+        Unknow.SetActive(true); //Esconder imagen
+    }
+
+    public void Show()
+    {
+        Unknow.SetActive(false);
+    }
+
+}
